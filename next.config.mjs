d74daad3278849c70eeba584eff800778e604aa8
Tanmay Sharma <tanmay@kali.tanmay>
2026-04-17 20:49:12 +0600

    Completed and fixed errors again

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "randomuser.me",
+    },
+  ],
+},
 };
 
 export default nextConfig;
